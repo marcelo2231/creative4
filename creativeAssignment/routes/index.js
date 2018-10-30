0038a28e2f8e14dfb911f32d67d474c021412180
Marcelo Archiza Almeida <archiza@hotmail.com>
2018-10-30 22:36:11 +0000

    Added the rest

--- creativeAssignment/routes/index.js
+++ creativeAssignment/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var unirest = require('unirest');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,10 +10,25 @@ router.get('/', function(req, res, next) {
 
 router.get('/api',function(req,res) {
     console.log("In api route");
-    var url = "https://owlbot.info/api/v1/dictionary/" + req.query.q;
+    console.log(req.query);
+    var url = "https://love-calculator.p.mashape.com/getPercentage?fname=" + req.query.q + "&sname=" + req.query.sname;
     console.log(url);
 
-    request(url).pipe(res);
-});
+    request({
+    headers: {
+      'X-Mashape-Key': "Tg9JnVn4oTmsh3JXwAJjIW0Ctg6Wp1QOx0kjsnINrtyIR5htIi"
+    },
+    uri: url,
+    method: 'POST'
+  }).pipe(res);
+
+  // These code snippets use an open-source library. http://unirest.io/nodejs
+//  unirest.get(url)
+//  .header("X-Mashape-Key", "Tg9JnVn4oTmsh3JXwAJjIW0Ctg6Wp1QOx0kjsnINrtyIR5htIi")
+//  .header("Accept", "application/json")
+//  .end(function (result) {
+//   //console.log(result.status, result.headers, result.body);
+//  });
+  });
 
 module.exports = router;
