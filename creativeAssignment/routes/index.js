4cf5a9aef58f1e4b6d913b438a84f533600959cb
Marcelo Archiza Almeida <archiza@hotmail.com>
2018-10-30 21:57:40 +0000

    Organized everything for an api call

--- creativeAssignment/routes/index.js
+++ creativeAssignment/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('weather.html', { root:  'public' });
+  res.sendFile('creative.html', { root:  'public' });
+});
+
+router.get('/api',function(req,res) {
+    console.log("In api route");
+    var url = "https://owlbot.info/api/v1/dictionary/" + req.query.q;
+    console.log(url);
+
+    request(url).pipe(res);
 });
 
 module.exports = router;
